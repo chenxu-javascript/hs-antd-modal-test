7e767d04fa67d56ef41b00d847e33fa6cd09d4de
弘树 <tiehang.lth@alibaba-inc.com>
2020-03-23 10:44:20 +0800

    init

---
+++ .history/src/lib/components/Button/Button_20200323103153.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button, Modal } from 'antd';
+
+export default () => {
+  const handleClick = () => {
+    Modal.info({
+      content: (
+        <div>
+          <h2>Hello</h2>
+          <p>BABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABA</p>
+        </div>
+      )
+    });
+  }
+  return (
+    <div>
+      <Button type='primary' onClick={handleClick}>点我</Button>
+    </div>
+  );
+}
